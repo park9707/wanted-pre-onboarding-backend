242a635706e2ba2e992b57406576c8dcabe0de4b
park9707 <sodkeldla@gmail.com>
2023-10-13 11:43:01 +0900

    init

---
+++ src/test/java/com/wantedpreonboardingbackend/jobs/JobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wantedpreonboardingbackend.jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
