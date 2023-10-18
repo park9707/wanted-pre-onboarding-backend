2aec6f12d86893bb6670335b65919bae9e53444b
park9707 <sodkeldla@gmail.com>
2023-10-18 22:12:50 +0900

    feat: 채용공고 삭제 기능 구현

--- src/main/java/com/wantedpreonboardingbackend/jobs/controller/NoticeController.java
+++ src/main/java/com/wantedpreonboardingbackend/jobs/controller/NoticeController.java
@@ -6,6 +6,8 @@ import com.wantedpreonboardingbackend.jobs.entity.Notice;
 import com.wantedpreonboardingbackend.jobs.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,5 +31,11 @@ public class NoticeController {
     @PutMapping
     public Notice updateNotice(@RequestBody NoticeUpdateRequestDto dto) {
         return noticeService.updateNotice(dto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{noticeId}")
+    public void deleteNotice(@PathVariable Long noticeId) {
+        noticeService.deleteNotice(noticeId);
     }
 }
